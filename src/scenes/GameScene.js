287dd2d278e6c1c809b1e720d28d048cef27b8fe
Birk <birk.kensen@gmail.com>
2021-11-18 20:11:34 +0100

    added running and jumping anim, and jumping restrictions

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,7 +1,12 @@
 import Phaser from "phaser";
 
-let player, controls, center, platform;
-// let gamePoints = 0;
+let player, center, platform;
+let gameOptions = {
+  jumps: 2,
+  jumpForce: 500,
+  playerGravity: 900,
+};
+// let playerJumps = 0;
 class GameScene extends Phaser.Scene {
   constructor() {
     super({
@@ -12,6 +17,7 @@ class GameScene extends Phaser.Scene {
   preload() {
     this.load.image("thanos", "./assets/thanospic.png");
     this.load.image("platform", "./assets/platform.png");
+    this.load.spritesheet("dude", "./assets/scaledDude.png", { frameWidth: 128, frameHeight: 128 });
   }
 
   create() {
@@ -20,31 +26,48 @@ class GameScene extends Phaser.Scene {
       y: this.physics.world.bounds.height / 2,
     };
 
-    player = this.physics.add.sprite(center.x - 100, 0, "thanos");
+    player = this.physics.add.sprite(center.x - 100, center.y, "dude");
+    // Set gravity
+    player.setGravityY(gameOptions.playerGravity);
+    // Define two animations, one for when running, one for jumping
+    this.anims.create({
+      key: "running",
+      frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+      frameRate: 12,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: "jumping",
+      frames: [{ key: "dude", frame: 0 }],
+    });
 
-    controls = this.input.keyboard.createCursorKeys();
-
+    this.input.on("pointerdown", this.jump, this);
+    this.playerJumps = 0;
+    // Create platform and set its physics
     platform = this.physics.add.sprite(center.x, center.y * 2 - 100, "platform");
-    platform.setCollideWorldBounds(true);
     platform.setImmovable(true);
     platform.body.moves = false;
-  }
 
+    this.physics.add.collider(player, platform);
+  }
   update() {
-    this.physics.add.collider(platform, player, landing, null, this);
-    if (controls.up.isDown) {
-      player.setVelocityY(-500);
-      console.log("up");
-    } else if (controls.down.isDown) {
-      player.setVelocityY(500);
-      console.log("down");
+    if (player.body.touching.down) {
+      player.anims.play("running", true);
+    }
+  }
+  jump() {
+    if (
+      player.body.touching.down ||
+      (this.playerJumps > 0 && this.playerJumps < gameOptions.jumps)
+    ) {
+      if (player.body.touching.down) {
+        this.playerJumps = 0;
+      }
+      player.setVelocityY(gameOptions.jumpForce * -1);
+      player.anims.play("jumping", true);
+      this.playerJumps++;
     }
   }
 }
 
 export default GameScene;
-
-function landing() {
-  player.setVelocityY(0);
-  console.log("test");
-}
