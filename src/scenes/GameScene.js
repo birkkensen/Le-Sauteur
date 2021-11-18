fb66318f32a8fee7a6911a5b0da4d9fef4941aba
Amin.Haghi <amin.haghi@hyperisland.se>
2021-11-19 00:01:32 +0100

    simple timer - start from 0 to 10

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,50 +1,72 @@
 import Phaser from "phaser";
 
-let player, controls, center, platform;
+let player, controls, center, platform, text, timedEvent;
+
+
+
+
 // let gamePoints = 0;
 class GameScene extends Phaser.Scene {
-  constructor() {
-    super({
-      key: "GameScene",
-    });
-  }
 
-  preload() {
-    this.load.image("thanos", "./assets/thanospic.png");
-    this.load.image("platform", "./assets/platform.png");
-  }
-
-  create() {
-    center = {
-      x: this.physics.world.bounds.width / 2,
-      y: this.physics.world.bounds.height / 2,
-    };
-
-    player = this.physics.add.sprite(center.x - 100, 0, "thanos");
-
-    controls = this.input.keyboard.createCursorKeys();
-
-    platform = this.physics.add.sprite(center.x, center.y * 2 - 100, "platform");
-    platform.setCollideWorldBounds(true);
-    platform.setImmovable(true);
-    platform.body.moves = false;
-  }
-
-  update() {
-    this.physics.add.collider(platform, player, landing, null, this);
-    if (controls.up.isDown) {
-      player.setVelocityY(-500);
-      console.log("up");
-    } else if (controls.down.isDown) {
-      player.setVelocityY(500);
-      console.log("down");
+    constructor() {
+        super({
+            key: "GameScene",
+        });
     }
-  }
+
+
+    preload() {
+        this.load.image("thanos", "./assets/thanospic.png");
+        this.load.image("platform", "./assets/platform.png");
+    }
+
+    create() {
+        center = {
+            x: this.physics.world.bounds.width / 2,
+            y: this.physics.world.bounds.height / 2,
+        };
+
+
+
+
+        player = this.physics.add.sprite(center.x - 100, 0, "thanos");
+
+        controls = this.input.keyboard.createCursorKeys();
+
+        platform = this.physics.add.sprite(center.x, center.y * 2 - 100, "platform");
+        platform.setCollideWorldBounds(true);
+        platform.setImmovable(true);
+        platform.body.moves = false;
+
+
+        //timer zero to 10
+        text = this.add.text(5, 15, 'timer');
+
+        timedEvent = this.time.delayedCall(10000, null);
+
+
+    }
+
+    update() {
+        this.physics.add.collider(platform, player, landing, null, this);
+
+        //timer
+        text.setText('T ' + timedEvent.getProgress().toString().substr(0, 5));
+
+        if (controls.up.isDown) {
+            player.setVelocityY(-500);
+            console.log("up");
+        } else if (controls.down.isDown) {
+            player.setVelocityY(500);
+            console.log("down");
+        }
+
+    }
 }
 
 export default GameScene;
 
 function landing() {
-  player.setVelocityY(0);
-  console.log("test");
+    player.setVelocityY(0);
+    console.log("test");
 }
