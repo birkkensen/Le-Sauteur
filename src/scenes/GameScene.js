1e381d00e862d79b3aaf84f831ff8c4f625847df
Birk <birk.kensen@gmail.com>
2021-11-26 13:21:35 +0100

    added start-and-end

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,24 +1,11 @@
 import Phaser from "phaser";
+import config from "../main.js";
 let game, timerText, ball;
 let counter = 0;
-window.onload = function () {
-  const config = {
-    type: Phaser.AUTO,
-    width: window.innerWidth,
-    height: window.innerHeight,
-    physics: {
-      default: "arcade",
-      arcade: {
-        debug: true,
-        gravity: { y: 0 },
-      },
-    },
-    scene: [GameScene],
-  };
 
+window.onload = () => {
   game = new Phaser.Game(config);
 };
-
 
 let gameOptions = {
   platformSpeedRange: [300, 300], //speed range in px/sec
@@ -43,20 +30,39 @@ class GameScene extends Phaser.Scene {
     });
   }
 
-  preload() {
-    this.load.image("platform", "./assets/platform.png");
-    this.load.image("ball", "./assets/redball.png");
-    this.load.spritesheet("dude", "./assets/thePlayer.png", { frameWidth: 96, frameHeight: 128 });
-    this.load.spritesheet("coins", "./assets/coins.png", { frameWidth:32, frameHeight:32 });
-  }
-
   create() {
-    center = {
+    this.center = {
       x: this.physics.world.bounds.width / 2,
       y: this.physics.world.bounds.height / 2,
     };
+    this.anims.create({
+      key: "background",
+      frames: this.anims.generateFrameNames("bg", {
+        start: 0,
+        end: 7,
+      }),
+      frameRate: 12,
+      repeat: -1,
+    });
+    this.bg = this.add.sprite(this.center.x, this.center.y, "bg");
+    this.bg.anims.play("background");
+    this.bg.setDisplaySize(this.center.x * 2 + 2, this.center.y * 2 + 2);
+    // setting player animation
+    this.anims.create({
+      key: "run",
+      frames: this.anims.generateFrameNumbers("dude", {
+        start: 0,
+        end: 3,
+      }),
+      frameRate: 8,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: "jump",
+      frames: [{ key: "dude", frame: 4 }],
+    });
 
-    this.anims.create({ 
+    this.anims.create({
       key: "rotate",
       frames: this.anims.generateFrameNumbers("coins", {
         start: 0,
@@ -65,8 +71,7 @@ class GameScene extends Phaser.Scene {
       frameRate: 20,
       repeat: -1,
     });
-    
-  
+
     // group with all active platforms.
     this.platformGroup = this.add.group({
       // once a platform is removed, it's added to the pool
@@ -75,7 +80,6 @@ class GameScene extends Phaser.Scene {
         platform.scene.platformPool.add(platform);
       },
     });
-    
 
     // platform pool
     this.platformPool = this.add.group({
@@ -88,85 +92,101 @@ class GameScene extends Phaser.Scene {
 
     //group all active coins
     this.coinGroup = this.add.group({
-      removeCallback: function(coin) { //once a coin is removed it's added to the pool 
+      removeCallback: function (coin) {
+        //once a coin is removed it's added to the pool
         // @ts-ignore Property 'coinPool' does not exist on type 'Scene'.
-        coin.scene.coinPool.add(coin)
-      }
+        coin.scene.coinPool.add(coin);
+      },
     });
 
     //coin pool
     this.coinPool = this.add.group({
-      removeCallback: function(coin) { //once a coin is removed from the pool it's added to the active coins group 
-       // @ts-ignore Property 'coinGroup' does not exist on type 'Scene'.
-        coin.scene.coinGroup.add(coin)
-      }
+      removeCallback: function (coin) {
+        //once a coin is removed from the pool it's added to the active coins group
+        // @ts-ignore Property 'coinGroup' does not exist on type 'Scene'.
+        coin.scene.coinGroup.add(coin);
+      },
     });
 
     //group all active bowling balls
     this.ballGroup = this.add.group({
-      removeCallback: function(ball) { //once a ball is removed it's added to the pool
+      removeCallback: function (ball) {
+        //once a ball is removed it's added to the pool
         // @ts-ignore Property 'ballPool' does not exist on type 'Scene'.
-        ball.scene.ballPool.add(ball)
-      }
+        ball.scene.ballPool.add(ball);
+      },
     });
 
     //ball pool
     this.ballPool = this.add.group({
-      removeCallback: function(ball) {
-      // @ts-ignore Property 'ballGroup' does not exist on type 'Scene'.
-        ball.scene.ballGroup.add(ball)
-      }
+      removeCallback: function (ball) {
+        // @ts-ignore Property 'ballGroup' does not exist on type 'Scene'.
+        ball.scene.ballGroup.add(ball);
+      },
     });
 
     // keeping track of added platforms
     this.addedPlatforms = 0;
     // number of consecutive jumps made by the player so far
-
-    // Add jump on spacebar
-    this.input.on("pointerdown", this.jump, this);
-
     this.playerJumps = 0;
     this.dying = false;
+
+    // Add jump on spacebar
+    this.input.keyboard.on("keydown-" + "SPACE", this.jump, this);
+    this.input.keyboard.on("keydown-" + "P", () => {
+      this.scene.pause();
+    });
 
     this.player = this.physics.add.sprite(
       gameOptions.playerStartPosition,
       game.config.height * 0.5,
       "dude"
     );
-    //setting collision between player and coins
-    this.physics.add.overlap(this.player, this.coinGroup, function(player, coin) {
-      this.tweens.add({
-        targets: coin,
-        // @ts-ignore
-        y: coin.y - 100,
-        alpha: 0,
-        duration: 800,
-        ease: "Cubic.easeOut",
-        callbackScope: this,
-        onComplete: function() {
-          this.coinGroup.killAndHide(coin);
-          this.coinGroup.remove(coin);
-        }
-      });
-    }, null, this);
-
-
-    //setting collision between player and bowling ball
-    this.physics.add.overlap(this.player, this.ballGroup, function(player, ball) {
-      this.tweens.add({
-        targets: ball,
-        // @ts-ignore
-        callbackScope: this,
-        onComplete: function() {
-          // this.removeHealth(ball);
-          // if removeHealth hit 3 times, this.dying = true; 
-        }
-      });
-    }, null, this);
-    
 
     this.player.setGravityY(gameOptions.playerGravity);
     this.player.setDepth(2);
+    this.player.setSize(48, 128);
+    //setting collision between player and coins
+    this.physics.add.overlap(
+      this.player,
+      this.coinGroup,
+      function (player, coin) {
+        this.tweens.add({
+          targets: coin,
+          // @ts-ignore
+          y: coin.y - 100,
+          alpha: 0,
+          duration: 800,
+          ease: "Cubic.easeOut",
+          callbackScope: this,
+          onComplete: function () {
+            this.coinGroup.killAndHide(coin);
+            this.coinGroup.remove(coin);
+          },
+        });
+      },
+      null,
+      this
+    );
+
+    //setting collision between player and bowling ball
+    this.physics.add.overlap(
+      this.player,
+      this.ballGroup,
+      function (player, ball) {
+        this.tweens.add({
+          targets: ball,
+          callbackScope: this,
+          onComplete: function () {
+            // this.removeHealth(ball);
+            // if removeHealth hit 3 times, this.dying = true;
+          },
+        });
+      },
+      null,
+      this
+    );
+
     // adding a platform to the game, the arguments are platform width, x position and y position
     this.addPlatform(
       game.config.width,
@@ -174,26 +194,30 @@ class GameScene extends Phaser.Scene {
       game.config.height * gameOptions.platformVerticalLimit[1]
     );
     // the player is not dying
-    this.dying = false,
-    // checking for input
-    this.input.on("pointerdown", this.jump, this),
-    // setting collisions between the player and the platform group
-    this.platformCollider = this.physics.add.collider(
-      this.player,
-      this.platformGroup,
-      function () {
-        // play "run" animation if the player is on a platform
-        if (!this.player.anims.isPlaying) {
-          this.player.anims.play("run");
-        }
-      },
-      null,
-      this
-    );
+    (this.dying = false),
+      // checking for input
+      // this.input.on("pointerdown", this.jump, this),
+      // setting collisions between the player and the platform group
+      (this.platformCollider = this.physics.add.collider(
+        this.player,
+        this.platformGroup,
+        function () {
+          // play "run" animation if the player is on a platform
+          if (!this.player.anims.isPlaying) {
+            this.player.anims.play("run");
+          }
+        },
+        null,
+        this
+      ));
 
     // this.physics.add.collider(player, platform);
-    timerText = this.add.text(100, 100, "points: 0");
-    timerText.setOrigin(0.5);
+    timerText = this.add.text(20, 20, "Points: 0", {
+      fontFamily: "Arcade",
+      fontSize: "16px",
+      color: "#000",
+    });
+    // timerText.setOrigin(, 1);
     this.time.addEvent({
       delay: 5000,
       callback: this.updateCounter,
@@ -239,10 +263,10 @@ class GameScene extends Phaser.Scene {
       gameOptions.platformSpawnRange[1]
     );
     //if this is not the starting platform?
-    if(this.addedPlatforms > 1) {
+    if (this.addedPlatforms > 1) {
       //if there is a coin over the platform?
-      if(Phaser.Math.Between(1, 100) <= gameOptions.coinPercent) {
-        if(this.coinPool.getLength()) {
+      if (Phaser.Math.Between(1, 100) <= gameOptions.coinPercent) {
+        if (this.coinPool.getLength()) {
           let coin = this.coinPool.getFirst();
           coin.x = posX;
           coin.y = posY - 96;
@@ -250,37 +274,40 @@ class GameScene extends Phaser.Scene {
           coin.active = true;
           coin.visible = true;
           this.coinPool.remove(coin);
-        }
-        else {
+        } else {
           let coin = this.physics.add.sprite(posX, posY - 96, "coins");
           coin.setImmovable(true);
           coin.setVelocityX(platform.body.velocity.x);
           // coin.anims.play("rotate");
-          coin.anims.play('rotate', true)
+          coin.anims.play("rotate", true);
           coin.setDepth(2);
           this.coinGroup.add(coin);
         }
       }
 
       //if there is a ball over the platform?
-      if(Phaser.Math.Between(1, 100) <= gameOptions.ballPercent) {
-        if(this.ballPool.getLength()) {
+      if (Phaser.Math.Between(1, 100) <= gameOptions.ballPercent) {
+        if (this.ballPool.getLength()) {
           ball = this.ballPool.getFirst();
           ball.x = posX - platformWidth / 2 + Phaser.Math.Between(1, platformWidth);
-          ball.y = posY - 52;
+          ball.y = posY - 32;
           ball.alpha = 1;
           ball.active = true;
           ball.visible = true;
           this.ballPool.remove(ball);
-        }
-        else {
-          ball = this.physics.add.sprite(posX - platformWidth / 2 + Phaser.Math.Between(1, platformWidth), posY - 52, "ball");
+        } else {
+          ball = this.physics.add.sprite(
+            posX - platformWidth / 2 + Phaser.Math.Between(1, platformWidth),
+            posY - 32,
+            "ball"
+          );
           ball.setImmovable(true);
           ball.setVelocityX(platform.body.velocity.x);
-          ball.setSize(8, 2)
-          ball.setScale(0.05)
+          ball.setOrigin(0.5, 1);
+          // ball.setSize(8, 2);
+          ball.setScale(0.25);
           //ball.anims.play create rotate animation
-          ball.setDepth(2)
+          ball.setDepth(2);
           this.ballGroup.add(ball);
         }
       }
@@ -288,7 +315,7 @@ class GameScene extends Phaser.Scene {
   }
   updateCounter() {
     counter++;
-    timerText.setText("points: " + counter);
+    timerText.setText("Points: " + counter);
   }
   jump() {
     if (
@@ -308,22 +335,61 @@ class GameScene extends Phaser.Scene {
     }
   }
 
-  
-
   update() {
-    this.physics.add.collider(platform, player, landing, null, this);
-    if (controls.up.isDown) {
-      player.setVelocityY(-500);
-      console.log("up");
-    } else if (controls.down.isDown) {
-      player.setVelocityY(500);
-      console.log("down");
+    // if the player is falliong down, reset
+    if (this.player.y > game.config.height) {
+      this.score = JSON.stringify(counter);
+      sessionStorage.setItem("Score", this.score);
+      counter = 0;
+      this.scene.start("Gameover");
+    }
+    // Keep the player at the same position on the x axis
+    this.player.x = gameOptions.playerStartPosition;
+    // recycling platforms
+    let minDistance = game.config.width;
+    let rightmostPlatformHeight = 0;
+    this.platformGroup.getChildren().forEach(function (platform) {
+      // @ts-ignore Property 'x' && 'displawidth' does not exist on type 'GameObject'.
+      let platformDistance = game.config.width - platform.x - platform.displayWidth / 2;
+      if (platformDistance < minDistance) {
+        minDistance = platformDistance;
+        // @ts-ignore Property 'y' does not exist on type 'GameObject'.
+        rightmostPlatformHeight = platform.y;
+      }
+      // @ts-ignore Property 'x' && 'displawidth' does not exist on type 'GameObject'.
+      if (platform.x < -platform.displayWidth / 2) {
+        this.platformGroup.killAndHide(platform);
+        this.platformGroup.remove(platform);
+      }
+    }, this);
+    // adding new platforms
+    if (minDistance > this.nextPlatformDistance) {
+      let nextPlatformWidth = Phaser.Math.Between(
+        gameOptions.platformSizeRange[0],
+        gameOptions.platformSizeRange[1]
+      );
+      let platformRandomHeight =
+        gameOptions.platformHeightScale *
+        Phaser.Math.Between(gameOptions.platformHeightRange[0], gameOptions.platformHeightRange[1]);
+      let nextPlatformGap = rightmostPlatformHeight + platformRandomHeight;
+      let minPlatformHeight = game.config.height * gameOptions.platformVerticalLimit[0];
+      let maxPlatformHeight = game.config.height * gameOptions.platformVerticalLimit[1];
+      let nextPlatformHeight = Phaser.Math.Clamp(
+        nextPlatformGap,
+        minPlatformHeight,
+        maxPlatformHeight
+      );
+      this.addPlatform(
+        nextPlatformWidth,
+        game.config.width + nextPlatformWidth / 2,
+        nextPlatformHeight
+      );
     }
 
     // adding new coins
-    this.coinGroup.getChildren().forEach(function(coin) {
+    this.coinGroup.getChildren().forEach(function (coin) {
       // @ts-ignore
-      if(coin.x < - coin.displayWidth / 2) {
+      if (coin.x < -coin.displayWidth / 2) {
         this.coinGroup.killAndHide(coin);
         this.coinGroup.remove(coin);
       }
@@ -335,9 +401,7 @@ class GameScene extends Phaser.Scene {
     //     this.removeHealth(ball);
     //   }
     // }, this);
-
   }
 }
 
 export default GameScene;
-
